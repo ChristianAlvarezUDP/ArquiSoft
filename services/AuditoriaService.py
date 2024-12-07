55c2afa6599df29f9c0779b0700b81a12f9a9751
ChristianAlvarezUDP <christian.alvarez_f@mail.udp.cl>
2024-12-07 19:46:45 -0300

    administrador

--- services/AuditoriaService.py
+++ services/AuditoriaService.py
@@ -19,55 +19,23 @@ def service_worker(service_name, host, port):
             print(data)
 
             response = ""
-            if data['comando'] == 'retrieve':
-                conn = sqlite3.connect("sqlite/arqui.db")
-                cursor = conn.cursor()
-                
-                cursor.execute(f'''
-                    SELECT * FROM auditoria
-                    ''')
-                result = cursor.fetchall()
-                print(f"Obtenido: {result}")
+            if data['comando'] == 'get_auditoria':
+                auditoria = get_auditoria(data['auditoria_id'])
+
+                result = {
+                    'status': 'correct',
+                    'auditoria': auditoria
+                }
+
+            elif data['comando'] == 'get_all_auditorias':
+                auditorias = get_all_auditorias()
+
+                result = {
+                    'status':  'correct',
+                    'auditorias': auditorias
+                }
+
                 response = json.dumps(result)
-            
-            if data['comando'] == 'add':
-                conn = sqlite3.connect("sqlite/arqui.db")
-                cursor = conn.cursor()
-                cursor.execute(f'''
-                    INSERT INTO auditoria VALUES (
-                        {data['body']['id']}, {data['body']['marca_temporal']}, {data['body']['fecha']}, {data['body']['id_grupo_campos']}, {data['body']['id_bus']}, {data['body']['id_tipo_auditoria']}, {data['body']['id_auditor']}
-                    )           
-                    ''')
-                result = conn.commit()
-                print(f"Obtenido: {result}")
-                response = json.dumps("Added one row")
-
-            if data['comando'] == 'register':
-                conn = sqlite3.connect("sqlite/arqui.db")
-                cursor = conn.cursor()
-                cursor.execute(f'''
-                    INSERT INTO auditoria VALUES (
-                        {data['body']['id']}, {data['body']['marca_temporal']}, {data['body']['fecha']}, {data['body']['id_grupo_campos']}, {data['body']['id_bus']}, {data['body']['id_tipo_auditoria']}, {data['body']['id_auditor']}
-                    )           
-                    ''')
-                result = conn.commit()
-                print(f"Obtenido: {result}")
-                response = json.dumps("Added one row")
-            if data['comando'] == 'login':
-                result = login(data['data']['user'], data['data']['password'])
-
-                if result:
-                    response_data = {
-                        'status': 'correct'
-                    }
-                
-                else:
-                    response_data = {
-                        'status': 'error',
-                        'mesage': 'Credenciales invalidas'
-                    }
-
-                response = json.dumps(response_data)
 
             else:
                 response_data = {
@@ -81,19 +49,43 @@ def service_worker(service_name, host, port):
             client_socket.close()
 
 
-def login(username, password):
+def get_auditoria(auditoria_id):
     conn = sqlite3.connect("sqlite/arqui.db")
     cursor = conn.cursor()
 
     cursor.execute(f'''
-        SELECT * FROM usuario
-        WHERE username = '{username}'
-        AND password = '{password}';
-        '''
-    )
+        SELECT * FROM auditoria AS a
+        JOIN grupo_campos AS gc ON a.id_grupo_campos = gc.id
+        JOIN bus AS b ON a.id_bus = b.id
+        JOIN tipo_auditoria AS ta ON a.id_tipo_auditoria = ta.id
+        JOIN auditor AS au ON a.id_auditor = au.id
+        JOIN campo_auditoria AS ca ON ca.id_auditoria = a.id
+        JOIN respuesta_auditoria AS ra ON ra.id_campo_auditoria = ca.id
+        WHERE a.id = (?)
+        ''', (auditoria_id,))
+    
+    result = cursor.fetchone
+    conn.close()
 
+    return result
+
+
+def get_all_auditorias():
+    conn = sqlite3.connect("sqlite/arqui.db")
+    cursor = conn.cursor()
+
+    cursor.execute(f'''
+        SELECT a.id, a.marca_temporal, a.fecha, gc.nombre, b.n_interno, ta.nombre, au.nombre FROM auditoria AS a
+        JOIN grupo_campos AS gc ON a.id_grupo_campos = gc.id
+        JOIN bus AS b ON a.id_bus = b.id
+        JOIN tipo_auditoria AS ta ON a.id_tipo_auditoria = ta.id
+        JOIN auditor AS au ON a.id_auditor = au.id
+        ''')
+    
     result = cursor.fetchall()
-    return len(result) > 0
+    conn.close()
+
+    return result
 
 
 if __name__ == '__main__':
