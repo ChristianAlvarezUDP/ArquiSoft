6aa32618188b851aec689ea600caad8da26f8779
eriquelme <eriquelme@softland.cl>
2024-12-07 15:38:34 -0300

    Cambiar nombres y expandir TODO

--- services/AuditoriaService.py
+++ services/AuditoriaService.py
@@ -1,8 +1,8 @@
 import sys
 import socket
 import threading
-import json
 import sqlite3
+import json
 
 def service_worker(service_name, host, port):
     print(f"{service_name} iniciando en {host}:{port}")
@@ -10,13 +10,15 @@ def service_worker(service_name, host, port):
         server_socket.bind((host, port))
         server_socket.listen()
         while True:
-            bus_socket, _ = server_socket.accept()
-            data = bus_socket.recv(1024).decode('utf-8')
+            client_socket, _ = server_socket.accept()
+            data = client_socket.recv(1024).decode('utf-8')
+            print(f"{service_name} received: {data}")
 
-            response = "Something failed"
-            data = json.loads(data)      
-            print(f"Data: {data}")
+            data = json.loads(data)
 
+            print(data)
+
+            response = ""
             if data['comando'] == 'retrieve':
                 conn = sqlite3.connect("sqlite/arqui.db")
                 cursor = conn.cursor()
@@ -51,21 +53,47 @@ def service_worker(service_name, host, port):
                 result = conn.commit()
                 print(f"Obtenido: {result}")
                 response = json.dumps("Added one row")
-            
-            
-            bus_socket.sendall(response.encode('utf-8'))
-            bus_socket.close()
+            if data['comando'] == 'login':
+                result = login(data['data']['user'], data['data']['password'])
+
+                if result:
+                    response_data = {
+                        'status': 'correct'
+                    }
+                
+                else:
+                    response_data = {
+                        'status': 'error',
+                        'mesage': 'Credenciales invalidas'
+                    }
+
+                response = json.dumps(response_data)
+
+            else:
+                response_data = {
+                    'status': 'error',
+                    'mesage': 'Comando incorrecto'
+                }
+
+                response = json.dumps(response_data)
+
+            client_socket.sendall(response.encode('utf-8'))
+            client_socket.close()
 
 
-def request(bus_host, bus_port, service_name, message):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((bus_host, bus_port))
-        request = f"{service_name}:{message}"
-        client_socket.sendall(request.encode('utf-8'))
-        response = client_socket.recv(1024)
-        print(f"Response: {response.decode('utf-8')}")
+def login(username, password):
+    conn = sqlite3.connect("sqlite/arqui.db")
+    cursor = conn.cursor()
 
-        return response.decode('utf-8')
+    cursor.execute(f'''
+        SELECT * FROM usuario
+        WHERE username = '{username}'
+        AND password = '{password}';
+        '''
+    )
+
+    result = cursor.fetchall()
+    return len(result) > 0
 
 
 if __name__ == '__main__':
