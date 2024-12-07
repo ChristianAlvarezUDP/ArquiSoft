38b0a06a1f54836db50e321d260c5cf5ce9e233d
Manuel Cáceres Farías <manuel.caceres_f@mail.udp.cl>
2024-12-07 13:08:04 -0300

    agregar digitador_client.py y crear addAuditorias_service.py

--- services/addAuditorias_service.py
+++ services/addAuditorias_service.py
@@ -13,9 +13,8 @@ def service_worker(service_name, host, port):
             bus_socket, _ = server_socket.accept()
             data = bus_socket.recv(1024).decode('utf-8')
 
-            response = ""
-            data = json.loads(data)
-            
+            response = "Something failed"
+            data = json.loads(data)      
             print(f"Data: {data}")
 
             if data['comando'] == 'retrieve':
@@ -28,7 +27,32 @@ def service_worker(service_name, host, port):
                 result = cursor.fetchall()
                 print(f"Obtenido: {result}")
                 response = json.dumps(result)
-     
+            
+            if data['comando'] == 'add':
+                conn = sqlite3.connect("sqlite/arqui.db")
+                cursor = conn.cursor()
+                cursor.execute(f'''
+                    INSERT INTO auditoria VALUES (
+                        {data['body']['id']}, {data['body']['marca_temporal']}, {data['body']['fecha']}, {data['body']['id_grupo_campos']}, {data['body']['id_bus']}, {data['body']['id_tipo_auditoria']}, {data['body']['id_auditor']}
+                    )           
+                    ''')
+                result = conn.commit()
+                print(f"Obtenido: {result}")
+                response = json.dumps("Added one row")
+
+            if data['comando'] == 'register':
+                conn = sqlite3.connect("sqlite/arqui.db")
+                cursor = conn.cursor()
+                cursor.execute(f'''
+                    INSERT INTO auditoria VALUES (
+                        {data['body']['id']}, {data['body']['marca_temporal']}, {data['body']['fecha']}, {data['body']['id_grupo_campos']}, {data['body']['id_bus']}, {data['body']['id_tipo_auditoria']}, {data['body']['id_auditor']}
+                    )           
+                    ''')
+                result = conn.commit()
+                print(f"Obtenido: {result}")
+                response = json.dumps("Added one row")
+            
+            
             bus_socket.sendall(response.encode('utf-8'))
             bus_socket.close()
 
