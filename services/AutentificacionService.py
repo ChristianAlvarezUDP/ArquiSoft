d67599ffff3181e3beb26ff58b3fd35d1a6853d6
ChristianAlvarezUDP <christian.alvarez_f@mail.udp.cl>
2024-12-07 16:09:57 -0300

    login

--- services/AutentificacionService.py
+++ services/AutentificacionService.py
@@ -17,36 +17,63 @@ def service_worker(service_name, host, port):
             data = client_socket.recv(1024).decode('utf-8')
 
             data = json.loads(data)
-            print(data)
-            response = ""
-
-            if data['comando'] == 'login':
-                response = request('db_service.py', json.dumps(data))
-
-            else:
-                response = {
-                    'status': 'error',
-                    'mesage': 'Comando incorrecto'
-                }
+            response = handle_command(data)
 
             print(f"{service_name} received: {data}")
             client_socket.sendall(response.encode('utf-8'))
             client_socket.close()
 
 
-def login(username, password):
+def handle_command(data):
+    if data['comando'] == 'login':
+        success = login(data["username"], data["password"], data["permisos"])
+
+        if success:
+            return {
+                'status': 'correct'
+            }
+        else:
+            return {
+                'status': 'error',
+                'message': 'Credenciales incorrectas'
+            }
+    
+    elif data['comando'] == 'get':
+        return get_users()
+    else:
+        return {
+            'status': 'error',
+            'message': 'Comando incorrecto'
+        }
+
+
+def login(username, password, permisos):
     conn = sqlite3.connect("sqlite/arqui.db")
     cursor = conn.cursor()
 
     cursor.execute(f'''
-        SELECT * FROM usuario
+        SELECT * FROM usuario AS u
+        JOIN grupo_usuario AS gu ON u.id_grupo = gu.id
         WHERE username = '{username}'
-        AND password = '{password}';
+        AND password = '{password}'
+        AND gu.nombre = {permisos};
         ''')
 
     result = cursor.fetchall()
     return len(result) > 0
 
+
+def get_users():
+    conn = sqlite3.connect("sqlite/arqui.db")
+    cursor = conn.cursor()
+
+    cursor.execute(f'''
+        SELECT * FROM usuario;
+        ''')
+    
+    result = cursor.fetchall()
+
+    return result
 
 
 if __name__ == '__main__':
